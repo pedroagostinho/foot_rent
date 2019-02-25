e03a2e8e6aea7a8602f8eb0330d5943c94cb3072
Pedro Agostinho <pedroagosti@gmail.com>
2019-02-25 18:42:55 +0000

    Data Seed Attempt 1

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :club
   belongs_to :player
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'byebug'
+
+Club.destroy_all
+Player.destroy_all
+Booking.destroy_all
+Review.destroy_all
+Stat.destroy_all
+
+number_of_clubs = 3
+number_of_players = 6
+number_of_bookings = 3
+number_of_reviews = 3
+
+number_of_clubs.times do
+  club = Club.new(
+    email: Faker::Internet.email,
+    password: "jonas10",
+    name: Faker::Football.team,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city
+  )
+  club.save!
+end
+
+
+number_of_players.times do
+  player = Player.new(
+    name: Faker::Football.player,
+    position: Faker::Football.position,
+    value_per_match: rand(100..100000),
+    nationality: Faker::Address.country,
+    age: rand(16..36),
+    availability: false,
+    club_id: rand(1..number_of_clubs)
+  )
+  player.save!
+end
+
+number_of_bookings.times do
+  booking = Booking.new(
+    club_id: rand(1..number_of_clubs),
+    player_id: rand(1..number_of_players),
+    start_date: "2019-02-25 17:30",
+    end_date: "2019-02-28 17:30"
+  )
+  booking.save!
+end
+
+number_of_reviews.times do
+  review = Review.new(
+    content: Faker::Lorem.paragraph,
+    stars: rand(0..5),
+    player_id: rand(1..number_of_players),
+    club_id: rand(1..number_of_clubs)
+  )
+  review.save!
+end
