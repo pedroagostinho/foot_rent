aaa625d7297413937d888850c25a76d7d11b4b91
Pedro Agostinho <pedroagosti@gmail.com>
2019-02-27 14:32:01 +0000

    player cards with logos

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'miguelbernal'
+  config.api_key = '845373226185834'
+  config.api_secret = 'jdxuWwxKPQqlG4sVXCAIlOGheE4'
+  config.secure = true
+  config.cdn_subdomain = true
+end
