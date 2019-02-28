79701e870fc13cdc90efb5064660cc0c807a6dee
Miguel Bernal <mdverabernal@gmail.com>
2019-02-28 12:23:53 +0000

    seeds improved, stats on player show

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,19 +25,19 @@ number_of_players.times do
     photo: URI("https://picsum.photos/100/50/?random"),
     name: Faker::Football.player,
     position: Faker::Football.position,
-    value_per_match: rand(100..1000),
+    value_per_match: rand(100...1000),
     nationality: Faker::Address.country,
-    age: rand(16...36),
+    age: rand(16..36),
     availability: true,
-    club_id: rand((Club.first.id)...(Club.last.id))
+    club_id: rand((Club.first.id)..(Club.last.id))
   )
   player.save!
 end
 
 number_of_bookings.times do
   booking = Booking.new(
-    club_id: rand((Club.first.id)...(Club.last.id)),
-    player_id: rand((Player.first.id)...(Player.last.id)),
+    club_id: rand((Club.first.id)..(Club.last.id)),
+    player_id: rand((Player.first.id)..(Player.last.id)),
     start_date: "2019-02-25 17:30",
     end_date: "2019-02-28 17:30"
   )
@@ -47,9 +47,9 @@ end
 number_of_reviews.times do
   review = Review.new(
     content: Faker::Lorem.paragraph,
-    stars: rand(0...5),
-    player_id: rand((Player.first.id)...(Player.last.id)),
-    club_id: rand((Club.first.id)...(Club.last.id))
+    stars: rand(0..5),
+    player_id: rand((Player.first.id)..(Player.last.id)),
+    club_id: rand((Club.first.id)..(Club.last.id))
   )
   review.save!
 end
@@ -58,9 +58,9 @@ list_id = Player.all.map { |p| p.id }
 
 list_id.each do |id|
   stat = Stat.new(
-    goals: rand(0...5),
-    assists: rand(0...5),
-    games_played: rand(0...5),
+    goals: rand(0..5),
+    assists: rand(0..5),
+    games_played: rand(0..5),
     form: ['low', 'medium', 'high'].sample,
     player_id: id,
   )
